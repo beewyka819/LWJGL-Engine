061918c33478d68d5f9178c67df44855006ef074
beewyka819 <psullivang7@gmail.com>
2019-12-21 15:39:37 -0500

    Update Mesh.java

--- src/main/java/com/webs/beewyka819/lwjglengine/engine/graph/Mesh.java
+++ src/main/java/com/webs/beewyka819/lwjglengine/engine/graph/Mesh.java
@@ -12,8 +12,6 @@ import com.webs.beewyka819.lwjglengine.engine.items.GameItem;
 import org.lwjgl.system.MemoryUtil;
 
 public class Mesh {
-    private static final int BITS_PER_BYTE = 8;
-
     private final int vaoID;
 
     private final int vboID;
@@ -39,8 +37,8 @@ public class Mesh {
             glBindBuffer(GL_ARRAY_BUFFER, vboID);
             glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
             glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-            glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, (Float.SIZE / BITS_PER_BYTE) * positions.length);
-            glVertexAttribPointer(2, 3, GL_FLOAT, false, 0, (Float.SIZE / BITS_PER_BYTE) * (positions.length + textureCoords.length));
+            glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, (Float.BYTES) * positions.length);
+            glVertexAttribPointer(2, 3, GL_FLOAT, false, 0, (Float.BYTES) * (positions.length + textureCoords.length));
 
             iboID = glGenBuffers();
             indicesBuffer = MemoryUtil.memAllocInt(indices.length);
